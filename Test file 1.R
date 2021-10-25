e1694afdc9a4b18e665a2b83590a32388a167fff
Ian Chng <ian@Ians-MacBook-Pro.local>
2021-10-25 23:06:35 +0100

    Third Commit. Remember to save first, otherwise you cannot commit

--- Test file 1.R
+++ Test file 1.R
@@ -3,3 +3,7 @@
 #Added a few more lines for a second commit, this time on GitHub
 #Lets see if this works
 #Check
+
+#Wow! It works!! Lets try one more time. This time I'm typing on R
+#Once more here. Does it work?
+#You gotta remember to save first before you can commit!
